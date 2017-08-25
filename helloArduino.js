47072b6e569d2071f52991384ebb50b75a191dca
marcin walendzik <marcin.walendzik@gmail.com>
2017-08-25 22:56:00 +0200

    Adding counter and stopping using Blink callback

--- helloArduino.js
+++ helloArduino.js
@@ -2,13 +2,26 @@ const five = require('johnny-five');
 const board = five.Board();
 
 const pin1 = 12;
-const pin2 = 13;
+// const pin2 = 13;
 
 board.on('ready',()=>{
 
+    let blinkCount = 0;
+    const blinkMax = 10;
+
     const led1 = new five.Led(pin1);
-    led1.blink(500);
-    const led2 = new five.Led(pin2);
-    led2.blink(1000);
+    // const led2 = new five.Led(pin2);
+
+    // led1.blink(500);
+    // led2.blink(1000);
+
+    led1.blink(500, () =>{
+       blinkCount++;
+       console.log(`Blinked: ${blinkCount}`);  // `` instead of ''
+       if(blinkCount >= blinkMax){
+           console.log('stopping..');
+           led1.stop();
+       }
+    });
 
 });
