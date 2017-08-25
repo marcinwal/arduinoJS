e9d9397f387729830e4fd043482f076525de758f
marcin walendzik <marcin.walendzik@gmail.com>
2017-08-25 22:20:04 +0200

    Tiny change for fun.

--- helloArduino.js
+++ helloArduino.js
@@ -1,7 +1,14 @@
 const five = require('johnny-five');
 const board = five.Board();
 
+const pin1 = 12;
+const pin2 = 13;
+
 board.on('ready',()=>{
-   const led = new five.Led(12);
-   led.blink(500);
+
+    const led1 = new five.Led(pin1);
+    led1.blink(500);
+    const led2 = new five.Led(pin2);
+    led2.blink(1000);
+
 });
